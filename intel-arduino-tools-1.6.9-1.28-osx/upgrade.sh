d27e5dce73af6c5373299b517824a3ecab97c464
YanMinge <myan@sierrawireless.com>
2017-01-04 10:34:02 +0800

    initial commit

---
+++ intel-arduino-tools-1.6.9-1.28-osx/upgrade.sh
@@ -0,0 +1,2 @@
+#！/bin/bash
+./arduino101load/arduino101load ./x86/bin  ./firmware/Blink.ino.arduino_101.hex /dev/cu.usbmodem00_01 verbose ATP1BLE000-1541C5635 141312
---
+++ intel-arduino-tools-1.6.9-1.28-windows/updateFirmware.bat
@@ -0,0 +1,2 @@
+.\arduino101load\arduino101load.exe .\x86\bin ..\firmware\arduino_101_firmware_A.hex COM9 verbose ATP1BLE000-1541C5635 141312
+pause
