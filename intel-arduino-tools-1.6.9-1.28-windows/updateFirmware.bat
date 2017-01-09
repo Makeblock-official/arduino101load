ad83d032dd47aa0c04d01893a82900a6d6a9790b
YanMinge <myan@sierrawireless.com>
2017-01-09 16:33:15 +0800

    update the firmware

--- intel-arduino-tools-1.6.9-1.28-windows/updateFirmware.bat
+++ intel-arduino-tools-1.6.9-1.28-windows/updateFirmware.bat
@@ -1,2 +1,2 @@
-.\arduino101load\arduino101load.exe .\x86\bin ..\firmware\arduino_101_firmware_A.hex COM9 verbose ATP1BLE000-1541C5635 141312
+.\arduino101load\arduino101load.exe .\x86\bin ..\firmware\arduino_101_firmware.hex COM9 verbose ATP1BLE000-1541C5635 141312
 pause
