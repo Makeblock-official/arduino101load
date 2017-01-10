b987ed482308dca2ff112b08089b98a35d5293d6
YanMinge <myan@sierrawireless.com>
2017-01-10 12:28:26 +0800

    change the file to bin

--- intel-arduino-tools-1.6.9-1.28-osx/upgrade.sh
+++ intel-arduino-tools-1.6.9-1.28-osx/upgrade.sh
@@ -1,2 +1,2 @@
 #！/bin/bash
-./arduino101load/arduino101load ./x86/bin  ./firmware/arduino_101_firmware.hex /dev/cu.usbmodem00_01 verbose ATP1BLE000-1541C5635 141312
+./arduino101load/arduino101load ./x86/bin  ./firmware/arduino_101_firmware.bin /dev/cu.usbmodem00_01 verbose ATP1BLE000-1541C5635 141312
